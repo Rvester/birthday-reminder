110879b1102e0a2e08ef3ab4e7fce753e7a0f4cc
Rvester <rasheem.khan94@gmail.com>
2024-11-06 14:10:54 -0500

    added scheduling for the local project

--- birthday_reminder.py
+++ birthday_reminder.py
@@ -1,7 +1,9 @@
 from birthday_checker import get_today_birthdays
 from email_sender import send_birthday_email
 from sms_sender import send_birthday_sms
-from coupon_generator import generate_coupon  
+from coupon_generator import generate_coupon
+import schedule 
+import time
 
 
 file_path = 'birthdays.csv'
@@ -24,3 +26,10 @@ for person in today_birthdays:
     
     if phone_number:
         send_birthday_sms(phone_number, name, coupon)
+
+
+# schedule.every().day.at("08:00").do(today_birthdays)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(60)
